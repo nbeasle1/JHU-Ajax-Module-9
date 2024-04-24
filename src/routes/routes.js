e970063b06de02995be6ca5896a6ea930a49f347
Shemp <shemplab@gmail.com>
2024-04-23 23:55:23 -0400

    try again

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -14,12 +14,12 @@
     
             .state('home', {
                 url: '/',
-                templateUrl: '../templates/home.template.html'
+                templateUrl: 'templates/home.template.html'
             })
     
             .state('categories', {
                 url: '/categories',
-                templateUrl: '../templates/categories.template.html',
+                templateUrl: 'templates/categories.template.html',
                 controller: 'CategoriesComponentController',
                 controllerAs: 'catCtrl',
                 resolve: {
@@ -31,7 +31,7 @@
             
             .state('items', {
                 url: '/items/{categoryShortName}',
-                templateUrl: '../templates/items.template.html',
+                templateUrl: 'templates/items.template.html',
                 controller: 'ItemsComponentController',
                 controllerAs: 'itemCtrl',
                 resolve: {
